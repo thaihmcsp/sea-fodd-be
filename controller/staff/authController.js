5a661f2855ed154820d87c39661b59ea56a78b59
thaidd7b <thaidd7b@gmail.com>
2022-11-19 13:38:03 +0700

    change base URL env

--- controller/staff/authController.js
+++ controller/staff/authController.js
@@ -1,30 +1,29 @@
 const userModel = require("../../models/userSchema");
 const { comparePassword } = require("../../services/auth");
-const jwt = require("jsonwebtoken");
-const jwtPass = process.env.jwt;
+const jwt = require('jsonwebtoken');
 
-exports.adminLogin = async function (req, res) {
+exports.staffLogin = async function (req, res) {
     try {
-        const { email, password } = req.body
+        const { email, password } = req.body;
         const userCheck = await userModel.findOne({ email });
         if (userCheck) {
             const matchPasswordUser = await comparePassword(password, userCheck.password);
             if (!matchPasswordUser) {
-                return res.json({ status: 'undifind password' });
+                return res.status(400).json({ status: 'undifind password' });
             } else if (userCheck && matchPasswordUser && userCheck.role === 'user') {
-                return res.json({ status: 'your account is not have permission' });
-            } else if (userCheck && matchPasswordUser && userCheck.role !== 'user') {
-                let token = jwt.sign({ id: userCheck._id }, jwtPass, { expiresIn: '90d' })
+                return res.status(400).json({ status: 'your account is not have permission' });
+            } else if (userCheck && matchPasswordUser && userCheck.role !== 'admin') {
+                let token = jwt.sign({ id: userCheck._id }, jwtPass, { expiresIn: '90d' });
                 await userModel.updateOne({ _id: userCheck._id }, { token });
-                res.json({
+                res.status(200).json({
                     data: { token: token, role: userCheck.role, userData: userCheck },
                     mess: 'oke',
                 })
             }
         } else {
-            return res.json({ status: 'email is not available' });
+            return res.status(400).json({ status: 'email is not available' });
         }
     } catch (error) {
-        res.json(error);
+        res.status(500).json(error);
     }
 }
--- router/index.js
+++ router/index.js
@@ -3,7 +3,10 @@ const path = require("path");
 const { checkToken } = require("../midderware/auth");
 const adminRouter = require("./adminRouter");
 const userRouter = require("./userRouter");
+const staffRouter = require("./staff/staffRouter");
+
 router.use("/admin", adminRouter);
 router.use("/user", userRouter);
+router.use('/staff', staffRouter);
 
 module.exports = router;
